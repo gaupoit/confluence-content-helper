bce78bef3f21cfd8aa244ec7dde7c607369062ed
gaupoit <gaupoit@outlook.com>
2016-04-18 19:19:30 +0800

    Ignore user configs file

---
+++ config/config.default.js
@@ -0,0 +1,9 @@
+{
+	"baseUrl": "http://confluence.niometrics.com",
+	"baseUri": "/rest/api/content",
+	"dataPath": "data",
+	"username": "jirabot",
+	"password": "eugo7ATeo0Aphoh",
+	"publicDir": "public",
+	"imagePath": "images"
+}
