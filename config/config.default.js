7af35e7a271031f6ac2831a51074bdc3e344b2e9
gaupoit <gaupoit@outlook.com>
2016-04-19 11:49:42 +0800

    Delete default config file

--- config/config.default.js
+++
@@ -1,9 +0,0 @@
-{
-	"baseUrl": "http://confluence.niometrics.com",
-	"baseUri": "/rest/api/content",
-	"dataPath": "data",
-	"username": "jirabot",
-	"password": "eugo7ATeo0Aphoh",
-	"publicDir": "public",
-	"imagePath": "images"
-}
